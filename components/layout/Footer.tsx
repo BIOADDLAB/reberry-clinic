012a5495614702134b5ba2e2b27710371b04eaac
유경아 <ukyunga0.1@gmail.com>
2026-07-10 11:01:31 +0900

    feat: 공통 컴포넌트 - footer 구현 & 반응형 대응

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,45 +4,54 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { site } from '@/components/lib/site';
 
+const links = [
+    { label: '비급여수가표', href: '#' },
+    { label: '이용약관', href: '#' },
+    { label: '개인정보처리방침', href: '#' },
+];
+
 export default function Footer() {
     return (
-        <footer>
-            <div className="bg-cocoa text-cream">
+        <footer className="bg-cocoa text-cream">
+            <div className="container-site py-14 text-center lg:pt-21.5 lg:pb-20">
                 {/* 로고 영역 */}
-                <h2>
-                    <Link href="/">
-                        <Image src="/images/logo.svg" alt={site.nameEn} width={176} height={18} />
-                    </Link>
-                </h2>
+                <Image
+                    src="/images/logo.svg"
+                    alt="RE:BERRY"
+                    width={150}
+                    height={28}
+                    className="notranslate mx-auto h-4.5 w-auto invert brightness-0 "
+                />
 
-                {/* 하단 메뉴 네비게이션 */}
-                <ul>
-                    <li>
-                        <Link href="/non-payment">비급여수가표</Link>
-                    </li>
-                    <li>
-                        <Link href="/terms">이용약관</Link>
-                    </li>
-                    <li>
-                        <Link href="/privacy">개인정보처리방침</Link>
-                    </li>
-                </ul>
+                {/* 이용약관 영역 */}
+                <nav className="mt-7.5 text-[15px] flex justify-center flex-wrap gap-x-8 gap-y-2 lg:gap-20">
+                    {links.map((i) => (
+                        <Link
+                            key={i.label}
+                            href={i.href}
+                            className="hover:opacity-80 duration-300 ease-in transition-all "
+                        >
+                            {i.label}
+                        </Link>
+                    ))}
+                </nav>
 
-                {/* 병원 정보 및 카피라이트 */}
-                <div>
-                    <div>
-                        <span>{site.name}</span>
-                        <span>|</span>
-                        <span>대표 : {site.ceo}</span>
-                        <span>|</span>
-                        <span>{site.address}</span>
-                        <span>|</span>
-                        <span>전화 : {site.tel}</span>
-                        <span>|</span>
-                        <span>사업자등록번호 : {site.businessNumber}</span>
-                    </div>
+                {/* 정보 영역 */}
+                <div className="mt-5.5 text-caption-xs flex justify-center flex-wrap items-center">
+                    {site.branch}
+                    <span className="mx-1.5 opacity">|</span>
+                    대표: {site.director}
+                    <span className="mx-1.5 opacity-40">|</span>
+                    {site.address}
+                    <span className="mx-1.5 opacity-40">|</span>
+                    전화: {site.tel}
+                    <span className="mx-1.5 opacity-40">|</span>
+                    사업자등록번호: {site.bizNo}
+                </div>
 
-                    <p>COPYRIGHT © 2026 REBERRY CLINIC ALL RIGHT RESERVED. Made By BIOADDLAB</p>
+                {/* 카피라이터 영역 */}
+                <div className="text-caption-xs uppercase leading-7">
+                    Copyright © 2026 Reberry Clinic All Right Reserved.
                 </div>
             </div>
         </footer>
--- components/lib/site.ts
+++ components/lib/site.ts
@@ -1,27 +1,22 @@
-// #COMPONENTS: 네비게이션, 병원 정보
-
 export const site = {
-    name: '리베리의원 마포점',
+    name: '리베리의원',
+    branch: '리베리의원 마포점',
     nameEn: 'RE:BERRY',
-    ceo: '유선민',
-    address: '서울특별시 마포구 월드컵로 200 11층',
-    address2: '서울특별시 마포구 월드컵로 200 11층 \n1101,1102호(성산동,SJ라벨라 상암)',
+    director: '유선민',
     tel: '02-2039-3777',
-    businessNumber: '876-48-01029',
-    reservation: '0507-1364-3777',
-    kakao: 'https://pf.kakao.com/_khIcn',
-    youtube: 'https://www.youtube.com/@%EB%A7%88%ED%8F%AC%ED%94%BC%EB%B6%80%EC%99%95',
-    naver: 'https://naver.me/5EniKFcp',
-    parking: '건물 뒷편 주차장 · 주차권 수령 후 \n지하 주차 전기차 주차 제한',
+    phone: '02 . 2039 . 3777',
+    phoneLink: 'tel:0220393777',
+    bizNo: '876-48-01029',
+    address: '서울특별시 마포구 월드컵로 200 11층',
+    addressDetail: '1101, 1102호 (성산동, SJ라벨라 상암)',
     subway: '6호선 마포구청역 1번 출구 도보 5분 이내',
+    parking: ['건물 뒷편 주차장 · 주차권 수령 후', '지하 주차, 전기차 주차 제한'],
+    kakao: 'https://pf.kakao.com/',
+    youtube: 'https://www.youtube.com/',
+    blog: 'https://blog.naver.com/',
+    naver: 'https://booking.naver.com/',
     hours: [
-        {
-            label: '평일',
-            value: '10:30-20:30',
-        },
-        {
-            label: '토요일',
-            value: '09:00-15:30',
-        },
+        { label: '평 일', value: '10:30 - 20:30' },
+        { label: '토요일', value: '09:00 - 15:30' },
     ],
 };
