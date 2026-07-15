811ab51de6687e992193aa3949a18ce13b3aa023
유경아 <ukyunga0.1@gmail.com>
2026-07-15 18:31:32 +0900

    wip: 시술결과 페이지 구현중

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,8 +1,14 @@
-// app/reviews/page.tsx
+import SubHero from '@/components/ui/SubHero';
+import LocationSection from '@/components/ui/LocationSection';
+
+export const metadata = { title: '시술결과' };
+
 export default function ReviewsPage() {
     return (
-        <main>
-            <h1>리뷰</h1>
-        </main>
+        <>
+            <SubHero en="Before &amp; After" image="/images/bg-sub-05.jpg" />
+
+            <LocationSection />
+        </>
     );
 }
