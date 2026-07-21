92eb69b6f12e74b0a0af4972f99a788ea6d36f81
유경아 <ukyunga0.1@gmail.com>
2026-07-21 11:06:07 +0900

    feat: column 데이터 추가

---
+++ components/lib/columns.ts
@@ -0,0 +1,45 @@
+export interface Col {
+    title: string;
+    en: string;
+    text: string;
+    link?: string;
+}
+
+export const columns: Col[] = [
+    {
+        title: '온다리프팅',
+        en: 'Onda',
+        text: '3mm vs 7mm\n내 얼굴엔 어떤 깊이가 맞을까?',
+        link: 'https://blog.naver.com/drpyton',
+    },
+    {
+        title: '피코토닝',
+        en: 'Pico',
+        text: '토닝은 많이 받을수록\n좋은 걸까?',
+        link: 'https://blog.naver.com/drpyton',
+    },
+    {
+        title: '스킨부스터',
+        en: 'Booster',
+        text: '리쥬란과 쥬베룩,\n무엇이 다를까?',
+        link: 'https://blog.naver.com/drpyton',
+    },
+    {
+        title: '울쎄라',
+        en: 'Ulthera',
+        text: '울쎄라와 써마지,\n내게 맞는 리프팅은?',
+        link: 'https://blog.naver.com/drpyton',
+    },
+    {
+        title: '여드름',
+        en: 'Acne',
+        text: '여드름 압출,\n해도 되는 걸까?',
+        link: 'https://blog.naver.com/drpyton',
+    },
+    {
+        title: '자외선',
+        en: 'UV',
+        text: '선크림만으로 기미를\n막을 수 있을까?',
+        link: 'https://blog.naver.com/drpyton',
+    },
+];
