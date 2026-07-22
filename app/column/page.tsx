a00736ba0e93dbb53cf4fba3e593c689985e8fd2
유경아 <ukyunga0.1@gmail.com>
2026-07-22 10:53:53 +0900

    feat: 피부칼럼 페이지 구현

--- app/column/page.tsx
+++ app/column/page.tsx
@@ -1,7 +1,25 @@
+import LocationSection from '@/components/ui/LocationSection';
+import SubHero from '@/components/ui/SubHero';
+import Image from 'next/image';
+
 export default function ColumnPage() {
     return (
         <main>
-            <h1>칼럼</h1>
+            <SubHero en="Column" image="/images/bg-sub-07.jpg" />
+            <section className="relative">
+                <Image
+                    src="/images/bg-texture-08.jpg"
+                    alt=""
+                    fill
+                    quality={80}
+                    sizes="100vw"
+                    className="object-cover"
+                />
+                <div className="container-site h-220  relative">
+                    <h2 className="text-center pt-40 text-h2 font-bold tracking-tight">닥터파이톤의 피부칼럼</h2>
+                </div>
+            </section>
+            <LocationSection />
         </main>
     );
 }
